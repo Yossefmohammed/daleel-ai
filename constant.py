adf0023a7bcc825ed3ac4e8b1a1b14c4a6ccf7a5
Yossefmohammed <ypssefmohammedahmed@gmail.com>
2026-02-16 21:11:01 +0200

    updated

--- constant.py
+++ constant.py
@@ -1,12 +1,10 @@
-from chromadb.config import Settings
 import os
 import tempfile
 
 # Use a subdirectory of the system temp directory – always writable
 DEFAULT_PERSIST_DIR = os.path.join(tempfile.gettempdir(), "wasla_chroma_db")
 
-CHROMA_SETTINGS = Settings(
-    chroma_db_impl="duckdb+parquet",
-    persist_directory=DEFAULT_PERSIST_DIR,
-    anonymized_telemetry=False
-)
+CHROMA_SETTINGS = {
+    "persist_directory": DEFAULT_PERSIST_DIR,
+    "anonymized_telemetry": False
+}
