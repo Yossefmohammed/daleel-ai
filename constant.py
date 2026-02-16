4b167cc7094b9abdabfed79ea28d80897549011d
Yossefmohammed <ypssefmohammedahmed@gmail.com>
2026-02-16 19:22:21 +0200

    updated

--- constant.py
+++ constant.py
@@ -1,15 +1,8 @@
-import os
 from chromadb.config import Settings
+import os
 
-if "STREAMLIT_CLOUD" in os.environ:
-    CHROMA_SETTINGS = Settings(
-        chroma_db_impl="duck_db+parquet",
-        persist_directory=None,  # in-memory
-        anonymized_telemetry=False
-    )
-else:
-    CHROMA_SETTINGS = Settings(
-        chroma_db_impl="duck_db+parquet",
-        persist_directory="db",  # local persistence
-        anonymized_telemetry=False
-    )
+CHROMA_SETTINGS = Settings(
+    chroma_db_impl="duck_db+parquet",
+    persist_directory="/tmp/db",  # Streamlit Cloud allows writing here
+    anonymized_telemetry=False
+)
